67d053c1b00baaa32cabb741763f1ebd9fa624ea
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-24 19:10:07 +0000

    Update MAX_NUM_FRAMES to 32 for better performance

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"]
 VIDEO_EXTENSIONS = [".mp4", ".mkv", ".mov", ".avi", ".flv", ".wmv", ".webm", ".m4v"]
-MAX_NUM_FRAMES = 64
+MAX_NUM_FRAMES = 32
 
 SYSTEM_MESSAGE = {
     "role": "system",
