aec444a4f960acd8ae801bad1dc69e15b00e278a
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-24 16:51:10 +0000

    adds ui components

---
+++ src/ui_components.py
@@ -0,0 +1,88 @@
+import concurrent.futures
+
+import streamlit as st
+
+from src.config import IMAGE_EXTENSIONS, VIDEO_EXTENSIONS
+from src.utils import encode_image, encode_video, all_images, all_videos
+
+
+@st.cache_data
+def cached_encode_image(image):
+    return encode_image(image)
+
+
+@st.cache_data
+def cached_encode_video(video):
+    return encode_video(video)
+
+
+def file_upload():
+    # Sidebar header
+    st.sidebar.header("Upload Files")
+    uploaded_files, file_objects = None, None
+
+    # File uploader with improved grammar and standardized code
+    uploaded_files = st.sidebar.file_uploader(
+        "Please select either up to 3 images or a single video file...",
+        type=IMAGE_EXTENSIONS + VIDEO_EXTENSIONS,
+        accept_multiple_files=True,
+    )
+
+    if uploaded_files is not None:
+        # check if all of the uploaded files are images or videos
+        if all_images(uploaded_files) and len(uploaded_files) <= 3:
+            st.sidebar.image(uploaded_files, use_column_width=True)
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                file_objects = list(executor.map(cached_encode_image, uploaded_files))
+
+        elif all_videos(uploaded_files) and len(uploaded_files) == 1:
+            with st.sidebar.status("Processing video..."):
+                file_objects = cached_encode_video(uploaded_files[0])
+                st.sidebar.video(uploaded_files[0])
+        else:
+            st.error(
+                "Please upload up to 3 images or a single video file with the following extensions: "
+                + ", ".join(IMAGE_EXTENSIONS + VIDEO_EXTENSIONS)
+            )
+
+    return uploaded_files, file_objects
+
+
+def header():
+    # CSS to center crop the image in a circle
+    circle_image_css = """
+    <style>
+    .center-cropped {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        border-radius: 50%;
+        width: 96px;
+        height: 96px;
+        object-fit: cover;
+    }
+    </style>
+    """
+
+    # Inject CSS
+    st.markdown(circle_image_css, unsafe_allow_html=True)
+
+    st.markdown(
+        """
+        <img src="https://azure.microsoft.com/en-us/blog/wp-content/uploads/2024/05/Azure_Blog_Isometric_Illustration-12_1260x708.jpg" class="center-cropped">
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.markdown(
+        "<h1 style='text-align: center;'>Chat with Phi 3.5/3 Vision LLMs</h1>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        "<div style='text-align: center; margin-bottom:4'>"
+        "Phi-3.5-vision is a lightweight, state-of-the-art open multimodal model built upon datasets which include synthetic data and filtered publicly available websites - with a focus on very high-quality, reasoning dense data both on text and vision. <a href='https://huggingface.co/microsoft/Phi-3.5-vision-instruct' target='_blank'>Read more</a>"
+        "<br>"
+        "<p>Made with ❤️ by <a href='https://github.com/bhimrazy' target='_blank'>Bhimraj Yadav</a></p>"
+        "</div>",
+        unsafe_allow_html=True,
+    )
